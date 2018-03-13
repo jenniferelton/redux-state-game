909a7b6ab74e6c97c5ccfde2a7050299872a6ca4
Jennifer Elton <justjenny40@hotmail.com>
2018-03-13 13:04:00 -0700

    fixed travis

--- src/components/app/reducers.test.js
+++ src/components/app/reducers.test.js
@@ -5,6 +5,6 @@ describe('game reducer', () => {
   
   it('should add a choice', () => {
     const state = game(undefined, { type: CHOICE, payload: { id: 1, activePlayer: 'O' } });
-    expect(state.squares[0]).toEqual('O');
+    expect(state.squares[1]).toEqual('O');
   });
 });
