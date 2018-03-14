f532b13a32e4179d4dd76c1a17c8c032b5850497
Jennifer Elton <justjenny40@hotmail.com>
2018-03-14 09:32:57 -0700

    added leaderboard component

--- src/components/app/Leaderboard.js
+++ src/components/app/Leaderboard.js
@@ -2,3 +2,20 @@ import React, { PureComponent } from 'react';
 import { connect } from 'react-redux';
 import { loadMatches } from '/actions';
 
+class GameHistory extends PureComponent {
+
+  render() {
+    return (
+      <section>
+        <ul>
+          <li key={match.key}>Score: {match.score}; Games: {match.games}</li>
+        </ul>
+      </section>
+    );
+  }
+}
+
+export default connect(
+  ({ matches }) => ({ matches }),
+  { loadMatches }
+) (GameHistory);
